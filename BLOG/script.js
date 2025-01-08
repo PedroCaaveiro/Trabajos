f0ebfbc36d3ff50df54cda348f433673c3c4d3e8
PedroCaaveiro <acaaveir@yahoo.es>
2025-01-08 12:28:14 +0100

    cambios

--- BLOG/script.js
+++ BLOG/script.js
@@ -1,10 +1,29 @@
-console.log(1);
-window.addEventListener("load",function(){
-    console.log(2);
 
-})
+const formulario = document.querySelector(".formulario");
+const parrafo = document.querySelector(".parrafo");
+formulario.addEventListener('submit',function (event){
+    event.preventDefault();
+    const nombre = document.getElementById("nombre").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const mensaje = document.getElementById("mensaje").value.trim();
+    
+    if (nombre === '' || email === '' || mensaje === '') {
+        parrafo.textContent = "Porfavor rellene los campos";
+    }else{
+        parrafo.textContent = " Se envio la informacion correctamente";
+    }
+
+});
 
 
 
 
-console.log(5);
+
+
+
+
+
+
+
+
+
